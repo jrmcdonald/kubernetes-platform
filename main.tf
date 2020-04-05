80b2a82a486dae81d122465c791b948caf4262e0
Jamie McDonald <jamie@qwyck.net>
2020-04-05 21:07:55 +0100

    build: pass variables to hobby kube module

--- main.tf
+++ main.tf
@@ -1,3 +1,12 @@
 module "hobby-kube" {
   source = "github.com/hobby-kube/provisioning"
+  
+  domain = "${var.domain}"
+  
+  hcloud_token    = "${var.hcloud_token}"
+  hcloud_ssh_keys = "${var.hcloud_ssh_keys}"
+  
+  
+  cloudflare_email     = "${var.cloudflare_email}"
+  cloudflare_api_token = "${var.cloudflare_api_token}"
 }
