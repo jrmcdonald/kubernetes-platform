92d4e6110feea5fa46ee7d3c37a25d036aabb553
Jamie McDonald <jamie@qwyck.net>
2020-04-06 19:22:18 +0100

    feat: add nginx-ingress

---
+++ modules/ingress/main.tf
@@ -0,0 +1,16 @@
+data "helm_repository" "stable" {
+  name = "stable"
+  url  = "https://kubernetes-charts.storage.googleapis.com"
+}
+
+resource "helm_release" "ingress" {
+  name       = "nginx-ingress"
+  repository = data.helm_repository.stable.metadata[0].name
+  chart      = "nginx-ingress"
+  version    = "0.30.0"
+  namespace  = "ingress"
+
+  values = [
+    "${file("values.yaml")}"
+  ]
+}
