f9a0590f535acefa486fe1e20eb635f7f3a68e4e
Jamie McDonald <jamie@qwyck.net>
2020-04-06 09:39:23 +0100

    build: fix formatting issues

--- main.tf
+++ main.tf
@@ -1,12 +1,11 @@
 module "hobby-kube" {
   source = "github.com/hobby-kube/provisioning"
-  
+
   domain = "${var.domain}"
-  
+
   hcloud_token    = "${var.hcloud_token}"
   hcloud_ssh_keys = "${var.hcloud_ssh_keys}"
-  
-  
+
   cloudflare_email     = "${var.cloudflare_email}"
   cloudflare_api_token = "${var.cloudflare_api_token}"
 }
