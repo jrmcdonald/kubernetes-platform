75b7ca306832ae3f02f5ea1d6797698d117a177f
Jamie McDonald <jamie@qwyck.net>
2020-04-14 13:27:29 +0100

    fix: fix nginx chart version

--- modules/helm/ingress.tf
+++ modules/helm/ingress.tf
@@ -2,7 +2,7 @@ resource "helm_release" "ingress" {
   name       = "nginx-ingress"
   repository = data.helm_repository.nginx-stable.url
   chart      = "nginx-ingress"
-  version    = "1.6.3"
+  version    = "0.4.3"
   namespace  = "ingress"
 
   values = [
