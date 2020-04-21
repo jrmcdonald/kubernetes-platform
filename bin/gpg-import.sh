d7d54ba7271bdb7e06f72e7a4695ec94f2b7c920
Jamie McDonald <jamie@qwyck.net>
2020-04-21 09:08:13 +0100

    build: refactoring post merge for pipeline deploy

--- bin/gpg-import.sh
+++ bin/gpg-import.sh
@@ -1,16 +1,22 @@
 #!/bin/bash
 
 # Required environment variables
-if [[ -z ${GPG_PUBKEY}  ]]; then
-  echo "Environment variable GPG_PUBKEY must be set"
-  exit 1
-fi
-  
-if [[ -z ${GPG_PRIVKEY}  ]]; then
-  echo "Environment variable GPG_PRIVKEY must be set"
+if [[ -z ${GPG_PUBKEY} ]]; then
+  printf >&2"Environment variable GPG_PUBKEY must be set\n"
   exit 1
 fi
 
-echo "Configuring GPG credentials"
+if [[ -z ${GPG_PRIVKEY} ]]; then
+  printf >&2 "Environment variable GPG_PRIVKEY must be set\n"
+  exit 1
+fi
+
+printf "Configuring GPG credentials"
 echo -e "${GPG_PUBKEY}" | gpg --import
 echo -e "${GPG_PRIVKEY}" | gpg --import
+
+printf "Listing imported keys\n"
+gpg --list-keys
+
+printf "Listing imported secret keys\n"
+gpg --list-secret-keys
--- bin/ssh-import.sh
+++ bin/ssh-import.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
 
 # Required environment variables
-if [[ -z ${SSH_PRIVKEY}  ]]; then
-  echo "Environment variable SSH_PRIVKEY must be set"
+if [[ -z ${SSH_AUTH_SOCK_DIR} ]]; then
+  printf >&2 "Environment variable SSH_AUTH_SOCK must be set\n"
   exit 1
 fi
 
-echo "Configuring SSH credentials"
-eval `ssh-agent`
-ssh-add - <<< "${SSH_PRIVKEY}"
+if [[ -z ${SSH_PRIVKEY} ]]; then
+  printf >&2 "Environment variable SSH_PRIVKEY must be set\n"
+  exit 1
+fi
+
+printf "Starting ssh-agent\n"
+eval $(ssh-agent -a ${SSH_AUTH_SOCK_DIR})
+
+printf "Adding ssh private key\n"
+ssh-add - <<<"${SSH_PRIVKEY}"
+
+printf "Listing adding keys\n"
+ssh-add -L
