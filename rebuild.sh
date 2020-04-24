b7c786d6477dd62b44fb5b502fb6ea4b0325d6ad
Jamie McDonald <jamie@qwyck.net>
2020-04-24 19:40:10 +0100

    build: set AWS_PROFILE when rebuilding

--- rebuild.sh
+++ rebuild.sh
@@ -1,4 +1,5 @@
 #/bin/sh
+export AWS_PROFILE=qwyck
 ansible-playbook -i localhost terraform.yaml --extra-vars "state=absent"
 ansible-playbook -i localhost terraform.yaml
 ansible-playbook -i production site.yaml --become
