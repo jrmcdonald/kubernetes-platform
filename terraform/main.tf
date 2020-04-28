34ce7d0b7bdefd2a0bd5e3649213e210860623e3
Jamie McDonald <jamie@qwyck.net>
2020-04-28 11:40:43 +0100

    refactor: fix terraform formatting

--- terraform/main.tf
+++ terraform/main.tf
@@ -33,13 +33,13 @@ module "dns" {
 module "inventory" {
   source = "./modules/ansible/inventory"
 
-  inventory_path = var.inventory_path
-  master_hostnames = module.provider.master_hostnames
+  inventory_path     = var.inventory_path
+  master_hostnames   = module.provider.master_hostnames
   master_private_ips = module.provider.master_private_ips
-  master_public_ips = module.provider.master_public_ips
-  worker_hostnames = module.provider.worker_hostnames
+  master_public_ips  = module.provider.master_public_ips
+  worker_hostnames   = module.provider.worker_hostnames
   worker_private_ips = module.provider.worker_private_ips
-  worker_public_ips = module.provider.worker_public_ips
+  worker_public_ips  = module.provider.worker_public_ips
 }
 
 module "group_vars" {
